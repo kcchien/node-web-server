88d31c95a832d70ff06690974bd4449d1519cd24
Chien Kuang Cheng <gzjian@gmail.com>
2017-01-24 15:29:24 +0800

    Express

--- server.js
+++ server.js
@@ -4,15 +4,79 @@
  */
 
 const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
 
-var app = express();
+const listenPort = 3000;
 
-app.get('/',(req, res) => {
-    res.send('First response');
+let app = express();
+
+// register partials template
+// before app.set
+hbs.registerPartials(__dirname + '/views/partials');
+
+// set view engine using hbs
+app.set('view engine', 'hbs');
+
+// use middleware
+// middleware mounted without a path will be executed for every request to the app.
+// 有request進來時會觸發
+// call next when done
+app.use((req, res, next) => {
+
+    let now = new Date().toString();
+    let log = `${now}: ${req.method} ${req.url}`;
+
+    console.log(log);
+
+    // 把LOG資訊寫入檔案
+    fs.appendFile('server.log',log + '\n', (err) => {
+        if(err){
+            console.log('Unable to append to server log.');
+        }
+    });
+    // 有呼叫next才會往下執行
+    next();
 });
 
-app.get('/about',(req, res) => {
-    res.send('First response');
+// use middleware to interrupt page load
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs');
+// });
+
+// serve static content
+// use middleware
+app.use(express.static(__dirname + '/public'));
+
+// register Helper
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
 });
 
-app.listen(3000);
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+    res.render('home.hbs',{
+        pageTitle:'Home Page',
+        welcomeMessage: 'Welcome to Home'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+    });
+});
+
+app.get('/bad', (req, res) => {
+    res.send({
+            errorMessage: 'Unable to handle request.'
+        }
+    );
+});
+
+app.listen(listenPort, () => {
+    console.log(`Server is running up on ${listenPort}`);
+});
